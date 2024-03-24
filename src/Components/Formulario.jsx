efb9185e08e70759acecc02799d109696be6c153
JORGE JOAQUIN RIVERA <jorgej.rivera@telefonica.com>
2024-03-24 16:04:08 -0300

    Formulario de contacto operativo.

---
+++ src/Components/Formulario.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import { useState } from "react";
+
+
+const Formulario = () => {
+	const [datosContacto, setdatosContacto] = useState({
+		nombre: "",
+		apellido: "",
+		telefono: "",
+		mail: ""
+	})
+	const [error, setError] = useState(false);
+	const [mostrar, setMostrar] = useState(false)
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		//validacion telefono
+		let regex = /^[0-9]*$/;
+		const telefonoValidado = regex.test(datosContacto.telefono);
+
+		//validacion mail.
+		regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		const mailValidado = regex.test(datosContacto.mail);
+
+		//Validar inputs.
+		if (datosContacto.nombre.length < 3
+			|| datosContacto.nombre.length > 20
+			|| datosContacto.apellido.length < 3
+			|| datosContacto.apellido.length > 20
+			|| !telefonoValidado
+			|| !mailValidado
+		) {
+			console.log("Hubo un error con su envio")
+			setError(true)
+		}
+		else {
+			setMostrar(true);
+			console.log("Se envio su mensaje, nos contactaremos a la novedad.")
+		}
+
+
+
+
+	}
+
+	//Aqui deberan implementar el form completo con sus validaciones
+	return (
+		<>
+		{error && <h3>Por favor verifique sus datos. </h3>}
+		{mostrar	?
+		<h2>Su mensaje fue recibido, nos contactaremos a la brevedad.</h2>
+		:
+		<form >
+			<div>
+				<label htmlFor="nombre">Nombre:</label>
+				<input
+					type="text"
+					id="nombre"
+					name="nombre"
+					placeholder="Ingresa tu nombre"
+					onChange={(e) => { setdatosContacto({ ...datosContacto, nombre: e.target.value }) }}
+					required
+				/>
+			</div>
+			<div>
+				<label htmlFor="apellido">Apellido:</label>
+				<input
+					type="text"
+					id="apellido"
+					name="apellido"
+					placeholder="Ingresa tu apellido"
+					onChange={(e) => { setdatosContacto({ ...datosContacto, apellido: e.target.value }) }}
+					required
+				/>
+			</div>
+			<div>
+				<label htmlFor="telefono">Teléfono:</label>
+				<input
+					type="tel"
+					id="telefono"
+					name="telefono"
+					placeholder="Ingresa tu telefono"
+					onChange={(e) => { setdatosContacto({ ...datosContacto, telefono: e.target.value }) }}
+					required
+				/>
+			</div>
+			<div>
+				<label htmlFor="mail">Email:</label>
+				<input
+					type="email"
+					id="mail"
+					name="mail"
+					placeholder="Ingresa tu Correo"
+					onChange={(e) => { setdatosContacto({ ...datosContacto, mail: e.target.value }) }}
+					required
+				/>
+			</div>
+			<button onClick={handleSubmit}>Enviar</button>
+		</form>
+		}
+		</>
+	);
+};
+export default Formulario;
--- src/Routes/Contacto.jsx
+++ src/Routes/Contacto.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import Form from '../Components/Form'
+import Formulario from '../Components/Formulario'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Contacto = () => {
   return (
-    <div>
-      <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
-      <Form/>
-    </div>
+    <>
+      <h2>Formulario de Contacto.</h2>
+      
+      <Formulario/>
+    </>
   )
 }
 
